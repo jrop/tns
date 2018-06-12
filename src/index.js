19463787526b30187c06882d0042f68a87c5cb73
Jonathan Apodaca <jrapodaca@gmail.com>
2018-06-12 16:50:39 -0600

    Add linting

--- src/index.js
+++ src/index.js
@@ -1,63 +1,72 @@
-import perplex from 'perplex'
+import perplex from "perplex";
 
 const keysToStrings = obj =>
-	Object.keys(obj).map(k => `${k}=${obj[k] && typeof obj[k].toString == 'function' ? obj[k].toString() : obj[k]}`)
+  Object.keys(obj).map(
+    k =>
+      `${k}=${
+        obj[k] && typeof obj[k].toString == "function"
+          ? obj[k].toString()
+          : obj[k]
+      }`
+  );
 
 class _Connections {
-	toString() {
-		return keysToStrings(this).join('\n')
-	}
+  toString() {
+    return keysToStrings(this).join("\n");
+  }
 }
 class _Object {
-	toString() {
-		return keysToStrings(this).map(part => `(${part})`).join('')
-	}
+  toString() {
+    return keysToStrings(this)
+      .map(part => `(${part})`)
+      .join("");
+  }
 }
 
 function parseConnection(lex) {
-	const name = lex.expect('WORD').match
-	lex.expect('=')
-	const desc = parseObject(lex)
-	return {name, desc}
+  const name = lex.expect("WORD").match;
+  lex.expect("=");
+  const desc = parseObject(lex);
+  return { name, desc };
 }
 
 function parseObject(lex) {
-	lex.expect('(')
-	const id = lex.expect('WORD').match
-	lex.expect('=')
+  lex.expect("(");
+  const id = lex.expect("WORD").match;
+  lex.expect("=");
 
-	let value
-	if (lex.peek().type == '(') {
-		// read properties
-		value = {}
-		Object.setPrototypeOf(value, _Object.prototype)
-		do {
-			Object.assign(value, parseObject(lex))
-		} while (lex.peek().type == '(')
-	} else
-		// simple value:
-		value = lex.expect('WORD').match
+  let value;
+  if (lex.peek().type == "(") {
+    // read properties
+    value = {};
+    Object.setPrototypeOf(value, _Object.prototype);
+    do {
+      Object.assign(value, parseObject(lex));
+    } while (lex.peek().type == "(");
+  }
+  // simple value:
+  else value = lex.expect("WORD").match;
 
-	lex.expect(')')
-	const result = {[id]: value}
-	Object.setPrototypeOf(result, _Object.prototype)
-	return result
+  lex.expect(")");
+  const result = { [id]: value };
+  Object.setPrototypeOf(result, _Object.prototype);
+  return result;
 }
 
 export default function parse(s) {
-	const lex = perplex(s)
-		.token('$SKIP_COMMENT', /#[^\n]*/)
-		.token('$SKIP_WS', /\s+/)
-		.token('WORD', /[a-z0-9\._-]+/i)
-		.token('(', /\(/)
-		.token(')', /\)/)
-		.token('=', /=/)
+  const lex = perplex(s)
+    .token("$SKIP_COMMENT", /#[^\n]*/)
+    .token("$SKIP_WS", /\s+/)
+    .token("WORD", /[a-z0-9._-]+/i)
+    .token("(", /\(/)
+    .token(")", /\)/)
+    .token("=", /=/);
 
-	const connections = {}
-	while (lex.peek().type != '$EOF') {
-		const conn = parseConnection(lex)
-		connections[conn.name] = conn.desc
-	}
-	Object.setPrototypeOf(connections, _Connections.prototype)
-	return connections
+  const connections = {};
+  while (lex.peek().type != "$EOF") {
+    const conn = parseConnection(lex);
+    connections[conn.name] = conn.desc;
+  }
+  Object.setPrototypeOf(connections, _Connections.prototype);
+  return connections;
 }
--- src/test.js
+++ src/test.js
@@ -1,46 +1,57 @@
-import parse from './index'
-import test from 'tape'
+import parse from "./index";
+import test from "tape";
 
-test('nested', t => {
-	t.deepLooseEqual({
-		'SOME_DB.CONN': {
-			DESCRIPTION: {
-				PROP1: '1',
-				PROP2: '2',
-				OBJ: {PROP3: '3'}
-			}
-		},
-		OTHER: {SHALLOW: 'true'}
-	}, parse(`
+test("nested", t => {
+  t.deepLooseEqual(
+    {
+      "SOME_DB.CONN": {
+        DESCRIPTION: {
+          PROP1: "1",
+          PROP2: "2",
+          OBJ: { PROP3: "3" }
+        }
+      },
+      OTHER: { SHALLOW: "true" }
+    },
+    parse(`
 		SOME_DB.CONN = (DESCRIPTION =
 			(PROP1 = 1)
 			(PROP2 = 2)
 			(OBJ = (PROP3 = 3)))
 		OTHER = (SHALLOW = true)
-	`))
-	t.end()
-})
+	`)
+  );
+  t.end();
+});
 
-test('WORDs (dashes)', t => {
-	t.deepLooseEqual({
-		ABCD: {
-			DESCRIPTION: {
-				ADDRESS_LIST: {
-					ADDRESS: {
-						PROTOCOL: 'TCP',
-						HOST: 'rac-scan.dbs.myorg.com',
-						PORT: '1521',
-					},
-				},
-				CONNECT_DATA: {SERVICE_NAME: 'ABCD'}
-			},
-		}
-	}, parse('ABCD=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=rac-scan.dbs.myorg.com)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=ABCD)))'))
-	t.end()
-})
+test("WORDs (dashes)", t => {
+  t.deepLooseEqual(
+    {
+      ABCD: {
+        DESCRIPTION: {
+          ADDRESS_LIST: {
+            ADDRESS: {
+              PROTOCOL: "TCP",
+              HOST: "rac-scan.dbs.myorg.com",
+              PORT: "1521"
+            }
+          },
+          CONNECT_DATA: { SERVICE_NAME: "ABCD" }
+        }
+      }
+    },
+    parse(
+      "ABCD=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=rac-scan.dbs.myorg.com)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=ABCD)))"
+    )
+  );
+  t.end();
+});
 
-test('toString', t => {
-	t.equal('ABC=(X=Y)', parse('ABC = (X = Y)').toString())
-	t.equal('ABC=(X=Y)\nDEF=(U=V)', parse('ABC = (X = Y) DEF = (U = V)').toString())
-	t.end()
-})
+test("toString", t => {
+  t.equal("ABC=(X=Y)", parse("ABC = (X = Y)").toString());
+  t.equal(
+    "ABC=(X=Y)\nDEF=(U=V)",
+    parse("ABC = (X = Y) DEF = (U = V)").toString()
+  );
+  t.end();
+});
